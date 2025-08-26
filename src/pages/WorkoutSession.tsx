62dccdb47f74792138bdf46e98e5046bab84374b
enigmashin3-png <enigma.shin3@gmail.com>
2025-08-26 04:50:37 +0200

    chore: fix react import in workout session

--- src/pages/WorkoutSession.tsx
+++ src/pages/WorkoutSession.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import ExerciseTypeahead from "../components/ExerciseTypeahead";
 import CountdownTimer from "../components/CountdownTimer";
 
