ceb01b97c0bc651306c811ac23560bd9fcb39543
enigmashin3-png <enigma.shin3@gmail.com>
2025-08-27 05:28:54 +0200

    Resolve server index conflict

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,10 +17,10 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-if (require.main === module) {
+if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 }
 
-module.exports = app;
+export default app;
